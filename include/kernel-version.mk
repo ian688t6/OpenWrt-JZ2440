93bfaa2847d5253f2ae338acf248417ade5f458e
gagovski <gagovski@163.com>
2015-02-05 19:52:03 +0800

    fisrt commit OpenWrt trunk

---
+++ include/kernel-version.mk
@@ -0,0 +1,30 @@
+# Use the default kernel version if the Makefile doesn't override it
+
+LINUX_RELEASE?=1
+
+LINUX_VERSION-3.8  = .13
+LINUX_VERSION-3.10 = .58
+LINUX_VERSION-3.13 = .7
+LINUX_VERSION-3.14 = .30
+LINUX_VERSION-3.18 = .5
+LINUX_VERSION-3.19 = -rc5
+
+LINUX_KERNEL_MD5SUM-3.8.13  = 2af19d06cd47ec459519159cdd10542d
+LINUX_KERNEL_MD5SUM-3.10.58 = 3ff3478b6351143cef22d4b81cf48b01
+LINUX_KERNEL_MD5SUM-3.13.7  = 370adced5e5c1cb1d0d621c2dae2723f
+LINUX_KERNEL_MD5SUM-3.14.30 = 5926982f20bbef0988fca03b92fe43a5
+LINUX_KERNEL_MD5SUM-3.18.5  = b655fcbc4597aa4b0cbabcfc72983259
+
+ifdef KERNEL_PATCHVER
+  LINUX_VERSION:=$(KERNEL_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_PATCHVER)))
+endif
+
+split_version=$(subst ., ,$(1))
+merge_version=$(subst $(space),.,$(1))
+KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
+KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
+KERNEL_PATCHVER ?= $(KERNEL)
+
+# disable the md5sum check for unknown kernel versions
+LINUX_KERNEL_MD5SUM:=$(LINUX_KERNEL_MD5SUM-$(strip $(LINUX_VERSION)))
+LINUX_KERNEL_MD5SUM?=x
